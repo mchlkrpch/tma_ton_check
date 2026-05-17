5e1a641d01547f4499ea9e55cefe2d2a4d06c054
mchlkrpch <krapchatov.ms@phystech.edu>
2026-05-17 03:44:52 +0300

    fix base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/tma-reactjs-template/',
+  base: '/tma_ton_check/',
   css: {
     preprocessorOptions: {
       scss: {
